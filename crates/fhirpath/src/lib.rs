cb93b5cceaae604bf33138bbb394207feeeb9e96
Steve Munini <smunini@gmail.com>
2025-02-28 22:07:07 -0500

    Initial FHIRPath tests are now passing

--- crates/fhirpath/src/lib.rs
+++ crates/fhirpath/src/lib.rs
@@ -2,19 +2,4 @@ mod parser;
 #[cfg(test)]
 mod tests;
 
-pub use parser::{Expression, Literal, Term, Invocation, parser};
-
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests_original {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub use parser::{parser, Expression, Invocation, Literal, Term};
