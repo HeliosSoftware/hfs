73ed8d53125bdbb3bd5f570c451b6a4a9eb6226f
aacruzgon <alan_cruz97@hotmail.com>
2026-04-17 00:05:53 -0400

    revert(fhirpath):   restore cfg-gated fallback arm in parse_fhir_resource

--- crates/fhirpath/src/handlers.rs
+++ crates/fhirpath/src/handlers.rs
@@ -547,7 +547,6 @@ fn detect_fhir_version(resource: &Value) -> FhirVersion {
 
 /// Parse FHIR resource based on version
 fn parse_fhir_resource(json: Value, version: FhirVersion) -> FhirPathResult<FhirResource> {
-    #[allow(unreachable_patterns)]
     match version {
         #[cfg(feature = "R4")]
         FhirVersion::R4 => {
@@ -573,8 +572,9 @@ fn parse_fhir_resource(json: Value, version: FhirVersion) -> FhirPathResult<Fhir
                 .map_err(|e| FhirPathError::InvalidInput(format!("Invalid R6 resource: {}", e)))?;
             Ok(FhirResource::R6(Box::new(resource)))
         }
+        #[cfg(not(any(feature = "R4", feature = "R4B", feature = "R5", feature = "R6")))]
         _ => Err(FhirPathError::InvalidInput(format!(
-            "FHIR version {:?} is not enabled in this helios-fhirpath build",
+            "FHIR version {:?} is not enabled",
             version
         ))),
     }
